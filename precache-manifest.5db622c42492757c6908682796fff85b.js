d10b771b1e4e21afe419fd2a00e9cb02f63c6e5d
Keyur <>
2020-02-23 16:37:49 +0800

    Updates

---
+++ precache-manifest.5db622c42492757c6908682796fff85b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfd05c4f852e8336518c8e8138ec28c",
+    "url": "/hivo-color-matcher/index.html"
+  },
+  {
+    "revision": "cfd457aee296cebf85d8",
+    "url": "/hivo-color-matcher/static/css/main.a5df429f.chunk.css"
+  },
+  {
+    "revision": "eca19fde5379c63c3f3c",
+    "url": "/hivo-color-matcher/static/js/2.8056f323.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/hivo-color-matcher/static/js/2.8056f323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd457aee296cebf85d8",
+    "url": "/hivo-color-matcher/static/js/main.d932bedc.chunk.js"
+  },
+  {
+    "revision": "0e33b6843afce83f0428",
+    "url": "/hivo-color-matcher/static/js/runtime-main.c2b4921e.js"
+  }
+]);
